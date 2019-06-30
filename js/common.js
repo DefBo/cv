3c2f98b30f326b6aa012b7d57a4b115536cbc341
Bogdan Andriiv <35779282+DefBo@users.noreply.github.com>
2019-06-30 17:14:36 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+// Get the root tag
+var root = document.getElementsByTagName( 'html' )[0];
+// Show navigation menu
+function showNav() {
+    root.classList.toggle('_show-nav');
+}
+
+window.addEventListener('load',function(){
+    setTimeout(addClassLoaded, 1000);
+
+    function addClassLoaded() {
+        document.querySelector('body').classList.add("loaded");
+    }
+});
