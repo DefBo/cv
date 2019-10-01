3c1f7bf5414b06f60a847f9a8c1d7f170c94bb7e
boandriiv <bo.andriiv@gmail.com>
2019-10-02 01:37:48 +0300

    fixes

--- js/common.js
+++ js/common.js
@@ -13,4 +13,47 @@ window.addEventListener('load',function(){
     }
 });
 
-window.onresize = function(){ location.reload(); }
+/*window.addEventListener('orientationchange', function() {
+    location.reload();
+}, false);*/
+
+window.addEventListener('orientationchange', function () {
+    var originalBodyStyle = getComputedStyle(document.body).getPropertyValue('display');
+    document.body.style.display='none';
+    setTimeout(function () {
+        document.body.style.display = originalBodyStyle;
+    }, 10);
+});
+
+/*
+function orientationChange() {
+    if(window.addEventListener) {
+        window.addEventListener("orientationchange", function() {
+            location.reload();
+        });
+    }
+}
+
+orientationChange();*/
+
+/*
+window.addEventListener('orientationchange', function () {
+    var originalBodyStyle = getComputedStyle (document.body).getPropertyValue('display');
+    document.body.style.display='none';
+    setTimeout(function () {
+        document.body.style.display = originalBodyStyle;
+    }, 10);
+});*/
+
+/*var supportsOrientationChange = "onorientationchange" in window,
+    orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+window.addEventListener(orientationEvent, function() {
+    window.location.reload()
+}, false);*/
+
+/*if (window.DeviceOrientationEvent) {
+    window.addEventListener('orientationchange', function() { location.reload(); }, false);
+}*/
+
+
